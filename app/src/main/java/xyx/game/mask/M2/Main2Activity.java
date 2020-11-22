d111ac0718bc1f263040fb9b057f3da8f3d4acb9
Administrator <316375076@qq.com>
2020-11-22 11:50:24 +0800

    11-22

--- app/src/main/java/xyx/game/mask/M2/Main2Activity.java
+++ app/src/main/java/xyx/game/mask/M2/Main2Activity.java
@@ -2,15 +2,114 @@ package xyx.game.mask.M2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.billingclient.api.BillingClient;
+import com.android.billingclient.api.BillingClientStateListener;
+import com.android.billingclient.api.BillingFlowParams;
+import com.android.billingclient.api.BillingResult;
+import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.PurchasesUpdatedListener;
+import com.android.billingclient.api.SkuDetails;
+import com.android.billingclient.api.SkuDetailsParams;
+import com.android.billingclient.api.SkuDetailsResponseListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import xyx.game.mask.R;
 
 public class Main2Activity extends AppCompatActivity {
 
+    // An activity reference from which the billing flow will be launched.
+    Activity activity = this;
+
+
+// Handle the result.
+
+    private PurchasesUpdatedListener purchasesUpdatedListener = new PurchasesUpdatedListener() {
+        @Override
+        public void onPurchasesUpdated(BillingResult billingResult, List<Purchase> purchases) {
+            // To be implemented in a later section.
+        }
+    };
+
+    private BillingClient billingClient = BillingClient.newBuilder(activity)
+            .setListener(purchasesUpdatedListener)
+            .enablePendingPurchases()
+            .build();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+        billingClient.startConnection(new BillingClientStateListener() {
+            @Override
+            public void onBillingSetupFinished(BillingResult billingResult) {
+                if (billingResult.getResponseCode() ==  BillingClient.BillingResponseCode.OK) {
+                    // The BillingClient is ready. You can query purchases here.
+                }
+            }
+            @Override
+            public void onBillingServiceDisconnected() {
+                // Try to restart the connection on the next request to
+                // Google Play by calling the startConnection() method.
+            }
+        });
+
+
+        //3
+        List<String> skuList = new ArrayList<>();
+        skuList.add("premium_upgrade");
+        skuList.add("gas");
+        final SkuDetailsParams.Builder params = SkuDetailsParams.newBuilder();
+        params.setSkusList(skuList).setType(BillingClient.SkuType.INAPP);
+        billingClient.querySkuDetailsAsync(params.build(),
+                new SkuDetailsResponseListener() {
+                    @Override
+                    public void onSkuDetailsResponse(BillingResult billingResult,
+                                                     List<SkuDetails> skuDetailsList) {
+                        // Process the result.
+                        // Retrieve a value for "skuDetails" by calling querySkuDetailsAsync().
+                        BillingFlowParams billingFlowParams = BillingFlowParams.newBuilder()
+                                .setSkuDetails(skuDetailsList.get(0))
+                                .build();
+                        int responseCode = billingClient.launchBillingFlow(activity, billingFlowParams).getResponseCode();
+
+                    }
+                });
+
+
+
+
+
+        Button button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                billingClient.querySkuDetailsAsync(params.build(),
+                        new SkuDetailsResponseListener() {
+                            @Override
+                            public void onSkuDetailsResponse(BillingResult billingResult,
+                                                             List<SkuDetails> skuDetailsList) {
+                                // Process the result.
+                                // Retrieve a value for "skuDetails" by calling querySkuDetailsAsync().
+                                BillingFlowParams billingFlowParams = BillingFlowParams.newBuilder()
+                                        .setSkuDetails(skuDetailsList.get(0))
+                                        .build();
+                                int responseCode = billingClient.launchBillingFlow(activity, billingFlowParams).getResponseCode();
+
+                            }
+                        });
+            }
+        });
     }
+
+
+
 }
